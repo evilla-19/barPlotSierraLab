4ac161f68b42cde2ca81d692bb924404ee148e7b
Eva Benito <benito.eva@gmail.com>
2020-01-09 18:48:45 +0100

    dependencies

--- rsconnect/shinyapps.io/evabenitogaragorri/init.R
+++ rsconnect/shinyapps.io/evabenitogaragorri/init.R
@@ -5,21 +5,16 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c('shiny',
-'rhandsontable',
-'Rmisc',
-'shinyMatrix',
-'ggplot2',
-'tidyverse',
-'officer',
-'rvg',
-'plyr',
-'export')
+my_packages = c('shiny','rhandsontable','Rmisc','shinyMatrix','ggplot2','tidyverse','officer','rvg','plyr','export')
+
+###########################################################
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
   }
 }
-
 invisible(sapply(my_packages, install_if_missing))
