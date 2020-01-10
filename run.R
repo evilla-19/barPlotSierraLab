16204994a0c9e98a9bcea8771eb12654547aadee
Eva Benito <benito.eva@gmail.com>
2020-01-10 21:38:27 +0100

    remove proc file

--- run.R
+++ run.R
@@ -2,10 +2,10 @@ library(shiny)
 
 port <- Sys.getenv('PORT')
 
-setwd(getwd())
+# setwd(getwd())
 
 shiny::runApp(
-#   appDir = getwd(),
+  appDir = getwd(),
   host = '0.0.0.0',
   port = as.numeric(port)
 )
