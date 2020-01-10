029b1fc29269162945120da84f0201f7ef6e7f94
Eva Benito <benito.eva@gmail.com>
2020-01-10 21:47:40 +0100

    local package installation

--- init.R
+++ init.R
@@ -18,3 +18,5 @@ install_if_missing = function(p) {
   }
 }
 invisible(sapply(my_packages, install_if_missing))
+
+install.packages("/barplot-demo/packages/export_0.2.2.tar.gz", repos=NULL, type="source")
