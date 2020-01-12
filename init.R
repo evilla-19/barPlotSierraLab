799d477bcdb2dafff4e7615f994ba20fb8c89986
Eva Benito <benito.eva@gmail.com>
2020-01-12 18:35:44 +0100

    reinstall export, rgl

--- init.R
+++ init.R
@@ -19,7 +19,7 @@ install_if_missing = function(p) {
 }
 invisible(sapply(my_packages, install_if_missing))
 
-# install.packages("/app/packages/rgl_0.100.30.tar.gz", repos=NULL, type="source")
-# install.packages("/app/packages/export_0.2.2.tar.gz", repos=NULL, type="source")
+install.packages("/app/packages/rgl_0.100.30.tar.gz", repos=NULL, type="source")
+install.packages("/app/packages/export_0.2.2.tar.gz", repos=NULL, type="source")
 
 # if (require("devtools")) install_github("INWTlab/shiny-matrix")
